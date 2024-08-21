6bcf2362f28b414ec30f85e479dd95f577ebcfd1
Subhasis-Sahu <subhasissahu10@gmail.com>
2024-08-21 19:43:00 +0530

    Update vsdbabysoc.v

--- src/module/vsdbabysoc.v
+++ src/module/vsdbabysoc.v
@@ -3,11 +3,10 @@ module vsdbabysoc (
    //
    input  wire reset,
    //
-   /*input  wire VCO_IN,
-   input  wire ENb_CP,*/
+   input  wire VCO_IN,
+   input  wire ENb_CP,
    input  wire ENb_VCO,
    input  wire REF,
-   	   
    //
    // input  wire VREFL,
    input  wire VREFH
@@ -22,18 +21,12 @@ module vsdbabysoc (
       .reset(reset)
    );
 
-   /*avsdpll pll (
+   avsdpll pll (
       .CLK(CLK),
       .VCO_IN(VCO_IN),
       .ENb_CP(ENb_CP),
       .ENb_VCO(ENb_VCO),
       .REF(REF)
-   );*/
-
-  pll pll_1 (
-      .f_vco(CLK),
-      .vctrl(ENb_VCO),
-      .f_clk_in(REF)
    );
 
    avsddac dac (
